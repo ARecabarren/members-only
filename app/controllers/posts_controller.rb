fe868c4d4761a26baabef2bd678c7f07a73de9e4
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-12-06 14:39:24 -0300

    Update posts controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,19 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index]
   
+  def index
+    @posts = Post.all
+  end
+
+  def show
+
+  end
+
   def new
   end
 
   def create
+
   end
 
-  def index
-  end
 end
