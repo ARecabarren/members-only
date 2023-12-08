ec96b8018f2bdef8df96b1bfe4212be108d36886
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-12-08 13:39:34 -0300

    Update authentication in PostsController and add username field in registration form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, only: [:new, :create]
   
   def index
     @posts = Post.all
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
