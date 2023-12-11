885fcc11f5bcd4b1b45e076607ac29ebfa402a97
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-12-11 12:20:16 -0300

    Add post creation functionality to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,17 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @posts = current_user.posts.build(post_params)
+    if @posts.save
+      redirect_to root_path, notice: "Post created successfully"
+    else
+      render :new
+    end
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
