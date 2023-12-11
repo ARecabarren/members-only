1a3c5b6358c234a59228eb544f2b8ae09cca625a
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-12-11 11:59:35 -0300

    Add user_id column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_163847) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_11_145722) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_163847) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
